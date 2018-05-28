c1e0db02612235d61fc7995a05289a975228e0a5
wysockey <twysockey@sjnma.org>
2018-05-28 09:25:08 -0500

    Test 1, saving pfc Wysockey

--- p1.py
+++ p1.py
@@ -1,12 +1,16 @@
 import math
-def is_square(number):
+Sqrt = int(raw_input("Enter number that is a perfect square.")
+
+if( [4,9,16,25,36,49,64,81,100,121,144] == Sqrt):
+	print("Its a perfect sqaure.")
+	else("Nope")
+
 	'''problem 1)
 	write a function that tells you if a number is a perfect square
 	return True when the variable number is a perfect square
 	return False when the variable called number is not a perfect square
 	e.g. 
-		number=16 -> True 
-		number =15 -> False
+	
 	'''
-	return True
+
  
